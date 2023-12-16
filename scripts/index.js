7a22e908e3f3c2c56596dccbbea2da9bc9081928
Aleksey Orekhov <aleoreh@gmail.com>
2023-12-16 20:44:44 +0300

    Повсеместной практикой является
    использование одинарных кавычек в JS коде

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 function deleteElement(element) {
     element.remove();
 }
 
 function createCardElement({ link, name }, deleteCardElement) {
-    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    const imageElement = cardElement.querySelector(".card__image");
-    const titleElement = cardElement.querySelector(".card__title");
+    const imageElement = cardElement.querySelector('.card__image');
+    const titleElement = cardElement.querySelector('.card__title');
     const deleteButtonElement = cardElement.querySelector(
-        ".card__delete-button"
+        '.card__delete-button'
     );
 
     imageElement.src = link;
     titleElement.innerText = name;
-    deleteButtonElement.addEventListener("click", () =>
+    deleteButtonElement.addEventListener('click', () =>
         deleteCardElement(cardElement)
     );
 
@@ -23,7 +23,7 @@ function createCardElement({ link, name }, deleteCardElement) {
 }
 
 function showCards(cards) {
-    const cardsListElement = document.querySelector(".places__list");
+    const cardsListElement = document.querySelector('.places__list');
 
     cards.forEach((card) => {
         cardsListElement.appendChild(createCardElement(card, deleteElement));
