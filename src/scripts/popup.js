5978e194aa3f2e14de8e96bb476a5802cebe0db6
Aleksey Orekhov <aleoreh@gmail.com>
2024-01-05 16:55:09 +0300

    Set show popup method ready

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,16 +1,49 @@
 function show(rootElement) {
+    const isOpenedClassName = 'popup_is-opened';
+
     const contentElement = rootElement.querySelector('.popup__content');
-    contentElement.addEventListener('click', (evt) => {
-        evt.stopPropagation();
-    });
-    rootElement.classList.add('popup_is-opened');
-    rootElement.addEventListener('click', () => {
-        rootElement.classList.remove('popup_is-opened');
-    });
     const closeButtonElement = rootElement.querySelector('.popup__close');
-    closeButtonElement.addEventListener('click', () => {
-        rootElement.classList.remove('popup_is-opened');
-    });
+    const firstInputElement = rootElement.querySelector('input');
+
+    function keydownHandler(evt) {
+        switch (evt.key) {
+            case 'Escape':
+                hideRootElement();
+                break;
+            default:
+                break;
+        }
+    }
+
+    function contentClickHandler(evt) {
+        // do not propagate click to the parent, as it closes when clicked
+        evt.stopPropogation();
+    }
+
+    function hideRootElement() {
+        rootElement.classList.remove(isOpenedClassName);
+
+        rootElement.removeEventListener('keydown', keydownHandler);
+        rootElement.removeEventListener('click', hideRootElement);
+        contentElement.removeEventListener('click', contentClickHandler);
+        closeButtonElement.removeEventListener('click', hideRootElement);
+    }
+
+    function showRootElement() {
+        rootElement.classList.add(isOpenedClassName);
+
+        rootElement.addEventListener('keydown', keydownHandler);
+        rootElement.addEventListener('click', hideRootElement);
+        contentElement.addEventListener('click', contentClickHandler);
+        closeButtonElement.addEventListener('click', hideRootElement);
+    }
+
+    function setFocus() {
+        (firstInputElement || closeButtonElement || rootElement).focus();
+    }
+
+    showRootElement();
+    setFocus();
 }
 
 export default { show };
