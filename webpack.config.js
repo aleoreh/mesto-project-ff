1eb76e5f4cfc5d374290082f287ee020320f0f15
Aleksey Orekhov <aleoreh@gmail.com>
2024-01-08 09:42:54 +0300

    лучше изображения и шрифты разместить в отдельных
    папках

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,21 @@ module.exports = {
                 exclude: '/node_modules/',
             },
             {
-                // регулярное выражение, которое ищет все файлы с такими расширениями
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]',
+                },
             },
             {
-                // применять это правило только к CSS-файлам
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash][ext]',
+                },
+            },
+            {
                 test: /\.css$/,
-                // при обработке этих файлов нужно использовать
-                // MiniCssExtractPlugin.loader и css-loader
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
@@ -50,3 +56,4 @@ module.exports = {
         new MiniCssExtractPlugin(),
     ],
 };
+
