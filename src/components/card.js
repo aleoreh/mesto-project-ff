3475ca636567001e8e96d21112b9f8eb4a071241
Aleksey Orekhov <aleoreh@gmail.com>
2024-02-06 23:00:08 +0300

    Add renderCardElement function

--- src/components/card.js
+++ src/components/card.js
@@ -1,47 +1,59 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const isLikedClassName = 'card__like-button_is-active';
 
-export function createCardElement(
-    { link, name, likes, owner, _id },
-    currentProfileId,
-    remove,
-    like,
-    show
-) {
+export function createCardElement(card, currentProfileId, remove, like, show) {
     const element = cardTemplate.querySelector('.card').cloneNode(true);
+    renderCardElement(element, card, currentProfileId);
 
     const imageElement = element.querySelector('.card__image');
-    const titleElement = element.querySelector('.card__title');
     const deleteButtonElement = element.querySelector('.card__delete-button');
     const toggleLikeElement = element.querySelector('.card__like-button');
-    const likesCountElement = element.querySelector('.card__likes-count');
 
-    element.setAttribute('data-card_id', _id);
-    imageElement.src = link;
-    imageElement.alt = generateAltImageText(name);
-    titleElement.innerText = name;
-    if (currentProfileId === owner._id) {
+    element.setAttribute('data-card_id', card._id);
+    if (currentProfileId === card.owner._id) {
         deleteButtonElement.addEventListener('click', () => remove(element));
         deleteButtonElement.setAttribute(
             'aria-label',
-            `Удалить карточку "${name}"`
+            `Удалить карточку "${card.name}"`
         );
     } else {
         deleteButtonElement.remove();
     }
-
     toggleLikeElement.addEventListener('click', () => {
-        like(toggleLikeElement);
+        like(element, currentProfileId);
     });
-    toggleLikeElement.setAttribute('aria-label', `Поставить like для: ${name}`);
-    imageElement.addEventListener('click', () => show({ link, name }));
-    likesCountElement.textContent = likes.length;
+    imageElement.addEventListener('click', () => show(card));
 
     return element;
 }
 
-export function toggleLike(toggleLikeElement) {
-    toggleLikeElement.classList.toggle(isLikedClassName);
+export function renderCardElement(
+    cardElement,
+    { link, name, likes },
+    currentProfileId
+) {
+    const hasOwnedLike = likes.some((like) => like._id === currentProfileId);
+
+    const imageElement = cardElement.querySelector('.card__image');
+    const titleElement = cardElement.querySelector('.card__title');
+    const toggleLikeElement = cardElement.querySelector('.card__like-button');
+    const likesCountElement = cardElement.querySelector('.card__likes-count');
+
+    cardElement.setAttribute('data-is_liked', JSON.stringify(hasOwnedLike));
+    imageElement.src = link;
+    imageElement.alt = generateAltImageText(name);
+    titleElement.innerText = name;
+    toggleLikeElement.classList.toggle(isLikedClassName, hasOwnedLike);
+    toggleLikeElement.setAttribute('aria-label', `Поставить like для: ${name}`);
+    likesCountElement.textContent = likes.length;
+}
+
+export function getCardId(cardElement) {
+    return cardElement.getAttribute('data-card_id');
+}
+
+export function getIsLiked(cardElement) {
+    return JSON.parse(cardElement.getAttribute('data-is_liked'));
 }
 
 export function removeCardElement(cardElement) {
